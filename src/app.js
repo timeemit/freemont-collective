ac0f9556e49be0682805a232a1876904df132e51
TimeEmit <LiamNorris1231853211@gmail.com>
2017-09-22 14:22:59 -0700

    Quotes: Use double quotes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import './styles/app.scss';
-import './images/figure0.jpg';
-import './images/figure1.jpg';
-import './images/figure2.jpg';
-import './images/figure3.jpg';
-import './images/figure4.jpg';
-import './images/figure5.jpg';
+import "./styles/app.scss";
+import "./images/figure0.jpg";
+import "./images/figure1.jpg";
+import "./images/figure2.jpg";
+import "./images/figure3.jpg";
+import "./images/figure4.jpg";
+import "./images/figure5.jpg";
 
