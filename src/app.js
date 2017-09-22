5fe2ed2ef85af047a90d1a8f771115d1865fcf86
TimeEmit <LiamNorris1231853211@gmail.com>
2017-09-22 15:36:04 -0700

    Display less crappy pngs.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import "./styles/app.scss";
-import "./images/figure0.jpg";
-import "./images/figure1.jpg";
-import "./images/figure2.jpg";
-import "./images/figure3.jpg";
-import "./images/figure4.jpg";
-import "./images/figure5.jpg";
+import "./images/figure0.png";
+import "./images/figure1.png";
+import "./images/figure2.png";
+import "./images/figure3.png";
+import "./images/figure4.png";
+import "./images/figure5.png";
 
