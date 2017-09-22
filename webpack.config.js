00968b2846adf89a7f4df24fb660716abc6f2e9e
TimeEmit <LiamNorris1231853211@gmail.com>
2017-09-22 16:24:10 -0700

    Stylesheets: pure.min.css

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
           use: ["css-loader", "sass-loader"]
